252cb17eea3972c85e1fa903bf999b9738b45235
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-07-31 15:07:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,9 @@
 def my_collect(array)
   new_array = []
   i = 0
-  while array.length > 0
+  while i < array.length  
     new_array << yield(array[i])
     i += 1
-    # binding.pry
+   
   end
-return new_array  ```return it here
-end
+return new_array  `
